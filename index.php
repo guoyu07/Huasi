586478edba2edbd5a6e35711ef2fa77b41bcc653
José Ignacio Guerrero VInueza <josevinguerrero@gmail.com>
2017-03-15 11:32:27 -0500

    favicon y Scrool
    *Se añadió el favicon a la pagina
    *Se añadió juqey y jQuery ui
    *Se animo el main-header al scrolear
    * se añadió la clase change en css

--- index.php
+++ index.php
@@ -5,16 +5,19 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Huasi | Style Guidelines</title>
+    <!--favicon-->
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
     <!--Resetear css de los navegadores-->
-    <link rel="stylesheet" href="/style/normalize.css">
+    <link rel="stylesheet" href="style/normalize.css">
     <!--Fuente para el proyecto-->
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,300italic' rel='stylesheet' type='text/css'>
     <!-- Estilos base-->
-    <link rel="stylesheet" href="/style/huasi.css">
+    <link rel="stylesheet" href="style/huasi.css">
   </head>
   <body>
     <!--Menu de navegación-->
-    <div class="main-header fixed">
+    <div class="main-header">
       <div class="header-logo">
         <a href="../">
           <?php echo file_get_contents("img/logo.svg"); ?>
@@ -29,27 +32,31 @@
       </div>
     </div>
 
-    <!-- Wrappewr-->
-    <div class="wrapper">
-      <div class="jumbotron full-height all-midle f-colum" id="top-hero">
-        <h2 class="hero-title">Quito es mucho más que una luz</h2>
-        <h4 class="hero-subtitle">Visitalo y sabras porque</h4>
-        <form>
-          <div class="form-busqueda">
-            <input type="text" placeholder="Lugar">
-            <input type="text" placeholder="Tiempo">
-            <input type="text" placeholder="Personas">
-            <button type="submit" name="button" class="btn btn-submit">Buscar</button>
-          </div>
-        </form>
-      </div>
+    <div class="jumbotron full-height all-midle f-colum" id="top-hero">
+      <h2 class="hero-title">Quito es mucho más que una luz</h2>
+      <h4 class="hero-subtitle">Visítalo y sabrás por qué</h4>
 
-      <div class="full-height">
-
-        
-
-      </div>
+      <!--Forma para buscar-->
+      <form>
+        <div class="form-busqueda">
+          <input type="text" placeholder="Lugar">
+          <input type="text" placeholder="Tiempo">
+          <input type="text" placeholder="Personas">
+          <button type="submit" name="button" class="btn btn-submit">Buscar</button>
+        </div>
+      </form>
     </div>
 
+    <!-- Wrappewr-->
+    <div class="wrapper">
+      <div class="full-heigh container">
+        <h2>Elementos</h2>
+        <h2 class="title">Titulo</h2>
+        <h2 class="subtitle">Sub titulo</h2>
+      </div>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+    <script src="js/jquery.js"></script>
   </body>
 </html>
