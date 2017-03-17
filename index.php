92b74280ee1b06d169ba97fbff38cb77a1bf45a2
José Ignacio Guerrero VInueza <josevinguerrero@gmail.com>
2017-03-16 23:56:44 -0500

    Footer y social icons
    Crear el html y css básico para el footer del sitio. Añadir los iconos
    soy de redes sociales

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
     <div class="main-header">
       <div class="header-logo">
         <a href="../">
-          <?php echo file_get_contents("img/logo.svg"); ?>
+          <?php echo file_get_contents("img/logo.svg");?>
           <h2>Huasi</h2>
         </a>
       </div>
@@ -49,10 +49,54 @@
 
     <!-- Wrappewr-->
     <div class="wrapper">
-      <div class="full-heigh container">
+      <div class="full-height container air-both">
         <h2>Elementos</h2>
         <h2 class="title">Titulo</h2>
         <h2 class="subtitle">Sub titulo</h2>
+      </div>
+    </div>
+
+    <div class="main-footer">
+      <div class="foot-section">
+        <div class="col-3">
+          <h2 class="subtitle">Titulo</h2>
+          <p>Elemento Elemento</p>
+          <p>Elemento Elemento</p>
+          <p>Elemento Elemento</p>
+          <p>Elemento Elemento</p>
+        </div>
+        <div class="col-3">
+          <h2 class="subtitle">Titulo</h2>
+          <p>Elemento Elemento</p>
+          <p>Elemento Elemento</p>
+          <p>Elemento Elemento</p>
+          <p>Elemento Elemento</p>
+        </div>
+        <div class="col-3">
+          <h2 class="subtitle">Titulo</h2>
+          <p>Elemento Elemento</p>
+          <p>Elemento Elemento</p>
+          <p>Elemento Elemento</p>
+          <p>Elemento Elemento</p>
+        </div>
+      </div>
+      <div class="foot-section">
+        <div>
+          <img src="img/logo.svg" alt="logo">
+          <h2 class="subtitle">Huasi</h2>
+        </div>
+        <div>
+          <a href="#"><p>Terminos y Privacidad</p></a>
+          <a href="#">
+            <?php echo file_get_contents("img/svg/facebook.svg");?>
+          </a>
+          <a href="#">
+          <?php echo file_get_contents("img/svg/twitter.svg");?>
+          </a>
+          <a href="#">
+          <?php echo file_get_contents("img/svg/instagram.svg");?>
+          </a>
+        </div>
       </div>
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
