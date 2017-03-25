62a994c62ae4414ac236e3ff2db4d123b367317f
José Ignacio Guerrero VInueza <josevinguerrero@gmail.com>
2017-03-25 16:36:15 -0500

    Hospedaje update

--- hospedaje.php
+++ hospedaje.php
@@ -35,14 +35,33 @@
 
   <div class="host-reserve container">
     <div class="nav col-8">
-      <a href="#"><div>Descripción</div></a>
-      <a href="#"><div>Evaluación</div></a>
-      <a href="#"><div>Ubicacion</div></a>
+      <a href="#descripcion"><div>Descripción</div></a>
+      <a href="#evaluaciones"><div>Evaluación</div></a>
+      <a href="#ubicacion"><div>Ubicacion</div></a>
     </div>
     <div class="col-3 all-middle">
       <h2><span class="big">$109.99</span> por Noche</h2>
       <div class="caption-reserve">
-
+        <form class="form form-reserva" action="index.html" method="post">
+          <div class="all-middle f-colum">
+            <div class="flex f-row">
+              <div class="flex f-colum">
+                <label for="llegada">LLegada</label>
+                <input type="text" name="llegada" placeholder="dd/mm/aa">
+              </div>
+              <div class="flex f-colum">
+                <label for="salida">Salida</label>
+                <input type="text" name="salida" placeholder="dd/mm/aa">
+              </div>
+            </div>
+            <div class="text-left">
+              <label for="huespedes">Huespedes</label>
+              <input type="text" name="huespedes" placeholder="1 huesped">
+            </div>
+          </div>
+          <button type="submit" class="btn btn-submit">Reservar</button>
+          <button type="button" class="btn btn-secondary">Añadir a WishList</button>
+        </form>
       </div>
     </div>
   </div>
@@ -53,13 +72,124 @@
       <div class="host-title col-12">
         <h2 class="subtitle">Hospedaje de Ejemplo</h2>
       </div>
-      <div class="flex f-row">
-        <div class="host-hero col-8">
+      <div class="flex f-colum">
+        <div class="host-info col-8">
           <img src="img/vp/vp-1.jpeg" alt="host" class="img-responsive">
+          <div id="descripcion">
+            <div class=" b-border margin-bottom"></div>
+            <h2 class="subtitle">Información</h2>
+            <p class="margin-bottom">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac velit fermentum, euismod purus at, gravida metus. Aenean et sollicitudin neque. Fusce leo mi, dignissim eget lectus at, luctus vulputate dui. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla id cursus enim, id gravida leo. Nulla urna elit, porttitor ac pulvinar ut, scelerisque ac libero. Nullam eu rhoncus diam.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac velit fermentum, euismod purus at, gravida metus. Aenean et sollicitudin neque. Fusce leo mi, dignissim eget lectus at, luctus vulputate dui. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla id cursus enim, id gravida leo. Nulla urna elit, porttitor ac pulvinar ut, scelerisque ac libero. Nullam eu rhoncus diam.
+            </p>
+            <div class="perks">
+              <h2 class="subtitle">Incluye</h2>
+              <ul>
+                <li>Ejemplo</li>
+                <li>Ejemplo</li>
+                <li>Ejemplo</li>
+                <li>Ejemplo</li>
+                <li>Ejemplo</li>
+                <li>Ejemplo</li>
+                <li>Ejemplo</li>
+                <li>Ejemplo</li>
+                <li>Ejemplo</li>
+                <li>Ejemplo</li>
+              </ul>
+            </div>
+          </div>
+          <div id="evaluaciones">
+            <div class=" b-border margin-bottom"></div>
+            <div class="flex">
+              <h2 class="subtitle">10 Evaluaciones</h2>
+              <span><img src="img/svg/icons/star.svg" alt=""></span>
+              <span><img src="img/svg/icons/star.svg" alt=""></span>
+              <span><img src="img/svg/icons/star.svg" alt=""></span>
+              <span><img src="img/svg/icons/star.svg" alt=""></span>
+              <span><img src="img/svg/icons/star.svg" alt=""></span>
+            </div>
+            <div class="host-coment">
+              <div class="flex f-row">
+                <img src="img/user/user-1.jpg" alt="">
+                <div class="coment-user">
+                  <h2 class="subtitle">Pedro Picapiedra</h2>
+                  <p>Marzo de 2017</p>
+                </div>
+              </div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac velit fermentum, euismod purus at, gravida metus. Aenean et sollicitudin neque. Fusce leo mi, dignissim eget.</p>
+            </div>
+            <div class="host-coment">
+              <div class="flex f-row">
+                <img src="img/user/user-4.jpg" alt="">
+                <div class="coment-user">
+                  <h2 class="subtitle">Bati Chica</h2>
+                  <p>Marzo de 2017</p>
+                </div>
+              </div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac velit fermentum, euismod purus at, gravida metus. Aenean et sollicitudin neque. Fusce leo mi, dignissim eget.</p>
+            </div>
+            <div class="host-coment">
+              <div class="flex f-row">
+                <img src="img/user/user-5.jpg" alt="">
+                <div class="coment-user">
+                  <h2 class="subtitle">Super Woman</h2>
+                  <p>Marzo de 2017</p>
+                </div>
+              </div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac velit fermentum, euismod purus at, gravida metus. Aenean et sollicitudin neque. Fusce leo mi, dignissim eget.</p>
+            </div>
+            <div class="host-coment">
+              <div class="flex f-row">
+                <img src="img/user/user-2.jpg" alt="">
+                <div class="coment-user">
+                  <h2 class="subtitle">Pablo Marmol</h2>
+                  <p>Marzo de 2017</p>
+                </div>
+              </div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac velit fermentum, euismod purus at, gravida metus. Aenean et sollicitudin neque. Fusce leo mi, dignissim eget.</p>
+            </div>
+          </div>
+          <div id="ubicacion">
+            <div class=" b-border margin-bottom"></div>
+            <h2 class="subtitle">Ubicación</h2>
+            <p>Av.Regresando al futuro 0e-212 y Rocafeller St</p>
+            <div class=" b-border margin-bottom"></div>
+          </div>
+        </div>
+
+        <div class="related-hosts all-middle f-colum">
+          <h2 class="subtitle">Hospedajes Similares</h2>
+          <div class="slide-show">
+            <a href="hospedaje.php" class="col-3">
+              <div class="host">
+                <div id="vp-1"></div>
+                <p>Ejemplo Habitación</p>
+                <p>$70</p>
+              </div>
+            </a>
+            <a href="hospedaje.php" class="col-3">
+              <div class="host">
+                <div id="vp-2"></div>
+                <p>Ejemplo Habitación</p>
+                <p>$70</p>
+              </div>
+            </a>
+            <a href="hospedaje.php" class="col-3">
+              <div class="host">
+                <div id="vp-3"></div>
+                <p>Ejemplo Habitación</p>
+                <p>$70</p>
+              </div>
+            </a>
+            <a href="hospedaje.php" class="col-3">
+              <div class="host">
+                <div id="vp-4"></div>
+                <p>Ejemplo Habitación</p>
+                <p>$70</p>
+              </div>
+            </a>
+          </div>
         </div>
       </div>
-      <div class="full-height"></div>
-      <div class="full-height"></div>
     </div>
   </div>
 
--- js/reserva.js
+++ js/reserva.js
@@ -1,34 +1,88 @@
-
-
-var $mainHeader = $(".main-header");
-var $mainNav = $(".main-nav");
-var $hostReserve = $(".host-reserve");
-var $navHostReserve = $(".host-reserve > .nav");
-var $captionReserve = $(".caption-reserve");
-var $mainFooter = $(".main-footer");
-
-function pageScroll(){
-
-  scrollStarts = $(this).scrollTop();
-  console.log("Host : "+($navHostReserve.offset().top + $captionReserve.height()));
-  console.log("Foot : " + $mainFooter.offset().top);
-  $mainHeader.css('position', 'absolute');
-  $hostReserve.css('position', 'absolute');
-
-  if(scrollStarts > $hostReserve.offset().top){
-    $hostReserve.css({'position':'fixed','top' : 0});
-  }else{
-    //añadir la clase change cuando el scrool sea mejor a 20.
-    $hostReserve.css({'top' : '60px','position':'absolute'});
-  }
-
-
-
 $(document).ready(function() {
 
-  //pageScroll();
-});
+  var $mainHeader = $(".main-header");
+  var $mainNav = $(".main-nav");
+  var $hostReserve = $(".host-reserve");
+  var $navHostReserve = $(".host-reserve > .nav");
+  var $captionReserve = $(".caption-reserve");
+  var $lastComentOffset;
+  var $navHostReserveOffset;
+  var $navHostReserveHeight;
+  var $navHostReserveBottom;
+  var $captionReserveOffset;
+  var $captionReserveHeight;
+  var $captionReserveBottom;
 
-$(document).scroll(function(){
-  pageScroll();
+  function updateVariables(){
+    $navHostReserveOffset = $navHostReserve.offset().top;
+    $navHostReserveHeight = $navHostReserve.height();
+    $navHostReserveBottom = $navHostReserveHeight + $navHostReserveOffset;
+    $captionReserveOffset = $captionReserve.offset().top;
+    $captionReserveHeight = $captionReserve.height();
+    $captionReserveBottom = $captionReserveOffset + $captionReserveHeight;
+    $lastComentOffset = $("#evaluaciones > .host-coment:last-of-type").offset().top;
+
+  }
+
+  function headerScroll(){
+
+
+    scrollStarts = $(this).scrollTop();
+    console.log("nav h: " + $navHostReserveBottom);
+    console.log("capt: " + $captionReserveOffset);
+
+    $mainHeader.css('position', 'absolute');
+    $hostReserve.css('position', 'absolute');
+    $captionReserve.css('top', $navHostReserveHeight);
+
+
+    if(scrollStarts > $hostReserve.offset().top){
+      $hostReserve.css({'position':'fixed','top' : 0});
+    }else{
+      //añadir la clase change cuando el scrool sea mejor a 20.
+      $hostReserve.css({'top' : '60px','position':'absolute'});
+    }
+
+  }
+
+  function captionScroll(){
+    scrollStarts = $(this).scrollTop();
+    console.log("nav h: " + $navHostReserveBottom);
+    console.log("capt: " + $captionReserveOffset);
+
+    if(scrollStarts >= $lastComentOffset - 300){
+      $captionReserve.css({'position':'absolute', 'top': $lastComentOffset});
+    }
+  }
+
+  function coolScroll(target, speed){
+    $('html, body').animate({
+      scrollTop: target.offset().top - $hostReserve.height()
+    }, speed);
+  }
+
+  //Coll Scroll
+  $('[href^="#"] ').click(function(){
+    var target = $($(this).attr('href'));
+    if(target.length) {
+      event.preventDefault();
+      coolScroll(target, 1000);
+    }
+  });
+
+
+
+  updateVariables();
+  captionScroll();
+  headerScroll();
+
+
+  $(document).scroll(function(){
+    updateVariables();
+    headerScroll();
+    captionScroll();
+  });
+
+
+
 });
